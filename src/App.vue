12c081d7dd7ab302868dbc2d6c27e66aa0ad54eb
seungupnoh <soungho70_@naver.com>
2020-06-19 17:53:18 +0900

    Vuex Mutations & Actions Part 2 done

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,36 @@
 <template>
   <v-app>
-    <v-app-bar app dark color="primary">
+    <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text
+        rounded
+        :to="link.url"
+      >
+        {{ link.label }}
+      </v-btn>
     </v-app-bar>
-
-    <!-- Login Module -->
     <v-content>
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="account_circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="lock"
-              :append-icon="showPassword ? 'visibility' : 'visibility_off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-content>
-
     <v-footer color="primary lighten-1" padless>
       <v-layout justify-center wrap>
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
+          :to="link.url"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-flex primary lighten-2 py-4 text-center white--text xs12>
-          {{ new Date().getFullYear() }} â€” <strong>Vuetify</strong>
+          {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
         </v-flex>
       </v-layout>
     </v-footer>
@@ -59,8 +42,20 @@ export default {
   name: "App",
   data() {
     return {
-      showPassword: false,
-      links: ["Home", "Login"],
+      links: [
+        {
+          label: "Home",
+          url: "/",
+        },
+        {
+          label: "Login",
+          url: "/login",
+        },
+        {
+          label: "Dashboard",
+          url: "/dashboard",
+        },
+      ],
     };
   },
 };
